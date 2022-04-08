02f492391e2b0249c2b8050255737c928e53a356
Dave Bechtum <dbechtum@gmail.com>
2022-04-08 21:18:12 +0200

    Attack animation and attack speed

--- Assets/Game/Scripts/Combat/Fighter.cs
+++ Assets/Game/Scripts/Combat/Fighter.cs
@@ -8,14 +8,17 @@ namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour, IAction
     {
-        [SerializeField]
-        float weaponRange = 2f;
+        [SerializeField] float weaponRange = 2f;
+        [SerializeField] float attackSpeed = 1.5f;
+        bool cooldown;
         Transform target;
         Mover mover;
+        Animator animator;
 
         private void Start()
         {
             mover = GetComponent<Mover>();
+            animator = GetComponent<Animator>();
         }
 
         private void Update()
@@ -28,8 +31,18 @@ namespace RPG.Combat
             else
             {
                 mover.Cancel();
+                AttackBehaviour();
             }
+        }
 
+        private void AttackBehaviour()
+        {
+            if (!cooldown)
+            {
+                animator.SetTrigger("attack");
+                cooldown = true;
+                Invoke("AttackCooldown", attackSpeed);
+            }
         }
 
         private bool GetIsInWeaponRange()
@@ -46,6 +59,17 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
+        }
+
+        private void AttackCooldown()
+        {
+            cooldown = false;
+        }
+
+        //Animation Event
+        private void Hit()
+        {
+
         }
     }
 }
