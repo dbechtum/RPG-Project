77cea075658daf4bfe58df92fb8b1c59de7eacea
Dave Bechtum <dbechtum@gmail.com>
2022-04-15 05:45:33 +0200

    Fix: Particles on projectiles no longer disappear instantly. Added: Option to list projectile effects that you wish to be destroyed immediately Added: Hit effect to projectiles

--- Assets/Game/Scripts/Combat/Projectile.cs
+++ Assets/Game/Scripts/Combat/Projectile.cs
@@ -14,7 +14,11 @@ namespace RPG.Combat
         [SerializeField] float lifeTime = 2f;
         [Tooltip("If is Homing, the projectile will continiously rotate towards the target.")]
         [SerializeField] bool isHoming = false;
+        [SerializeField] GameObject hitEffect = null;
         float damage = 0f;
+
+        [Tooltip("All gameobjects to be destroyed immediately, upon impact. The rest will be destroyed after particles have run out.")]
+        [SerializeField] GameObject[] destroyListOnImpact = null;
 
         // Update is called once per frame
         void Update()
@@ -55,10 +59,32 @@ namespace RPG.Combat
 
                 }
             }
-            KillProjectile();
+            if (hitEffect != null) Instantiate(hitEffect, transform.position, transform.rotation);
+            StopProjectile();
         }
 
-        private void KillProjectile()
+        //Stops particles before destroying the projectile
+        private void StopProjectile()
+        {
+            gameObject.GetComponent<Collider>().enabled = false; //disable collision.
+            foreach (GameObject child in destroyListOnImpact) //destroy list of objects that should be hidden right away.
+            {
+                Destroy(child);
+            }
+            float destroyDelay = 0f;
+            ParticleSystem[] particles = gameObject.GetComponentsInChildren<ParticleSystem>();
+            foreach (ParticleSystem particle in particles) //disable particles, and track which has the longest duration.
+            {
+                destroyDelay = (particle.main.duration > destroyDelay)
+                    ? particle.main.duration
+                    : destroyDelay;
+                particle.Stop();
+            }
+            Invoke("DestroyProjectile", destroyDelay); // destroy the projectile after particles have died.
+        }
+
+        //Completely removes the projectile immediately (any remaining particles will despawn immediately)
+        private void DestroyProjectile()
         {
             Destroy(gameObject);
         }
